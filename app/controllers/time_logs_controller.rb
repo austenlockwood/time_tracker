cc3f4af55739fe2f95e26f841f9f03f4d9d0a670
Austen Lockwood <austenlockwood@gmail.com>
2016-08-23 11:45:54 -0400

    added functionality so all users can make and edit time entries

--- app/controllers/time_logs_controller.rb
+++ app/controllers/time_logs_controller.rb
@@ -1,2 +1,38 @@
 class TimeLogsController < ApplicationController
+
+  def index
+    @time_logs = TimeLog.all
+  end
+
+  def new
+    @time_log = TimeLog.new
+  end
+
+  def create
+    @time_log = TimeLog.new(time_log_params)
+    if @time_log.save
+      redirect_to time_logs_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @time_log = TimeLog.find(params["id"])
+  end
+
+  def update
+    @time_log = TimeLog.find(params["id"])
+
+    if @time_log.update(time_log_params)
+      redirect_to time_logs_path
+    else
+      render :edit
+    end
+  end
+
+  private def time_log_params
+    params.require("time_log").permit(:developer_id, :date, :minutes, :project_id)
+  end
+
 end
