83e2cb1147dd5ecbcc0b0a0de38a435d797fb161
Austen Lockwood <austenlockwood@gmail.com>
2016-08-23 21:02:30 -0400

    added BCrypt gem, bundled, and added has_secure_password to developer model so users must have a password, password can be up to 72 characters, and password will be confirmed

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,5 +1,6 @@
 class Developer < ActiveRecord::Base
-has_many :time_logs
-has_many :projects, through: :time_logs
-validates :email, uniqueness: true, presence: true
+  has_many :time_logs
+  has_many :projects, through: :time_logs
+  validates :email, uniqueness: true, presence: true
+  has_secure_password
 end
