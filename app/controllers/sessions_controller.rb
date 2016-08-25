d804b4dde17082468a31dd927ce3552556863ff5
Austen Lockwood <austenlockwood@gmail.com>
2016-08-25 09:05:56 -0400

    added link on root route/welcome page  to sessions_new route/log in page; created 'Welcome, back [name]' message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,22 @@
 class SessionsController < ApplicationController
-  #show form
+  # show form
   def new
   end
 
-  #do work when they submit form
+  # do work when they submit form
   def create
-    # @developer = Developer.find_by(email: "russell@tiy.com")
+    # @developer = search database for email that was typed in
     @developer = Developer.find_by(email: params[:email])
-    #they log in right
-    if @developer.authenticate(params[:password])
+    # if they log in right:
+    if @developer && @developer.authenticate(params[:password])
       # need to be remembered as logged in on all pages
-      session[:developer_id] = @developer.id
+      session[:developer_welcome] = @developer.name
       # session[:its_a_party_key] = "You logged in dude"
       #they get a welcome page REDIRECT
-      redirect_to root_path
+      redirect_to root_path, notice: "Welcome back, #{@developer.name}!"
     else
-      render :new, notice: "Password or username wrong. Try again."
+      flash[:notice] = 'Password or username wrong. Try again.'
+      render :new
     end
   end
 
